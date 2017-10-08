42695149cfd3b32b2b7f8baec2a49765f93aaa25
linux <tomnomnom1@gmail.com>
2017-10-08 23:53:21 +0100

    changing the model around

--- model.py
+++ model.py
@@ -57,34 +57,46 @@ def fc(scope_name, inputs, shape, bias_shape, wd=0.04, reuse=False, trainable=Tr
 
 
 def inference(images, reuse=False, trainable=True):
-    coarse1_conv = conv2d('coarse1', images, [11, 11, 3, 96], [96], [1, 4, 4, 1], padding='VALID', reuse=reuse, trainable=trainable)
-    coarse1 = tf.nn.max_pool(coarse1_conv, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name='pool1')
-    coarse2_conv = conv2d('coarse2', coarse1, [5, 5, 96, 256], [256], [1, 1, 1, 1], padding='VALID', reuse=reuse, trainable=trainable)
-    coarse2 = tf.nn.max_pool(coarse2_conv, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool1')
-    coarse3 = conv2d('coarse3', coarse2, [3, 3, 256, 384], [384], [1, 1, 1, 1], padding='VALID', reuse=reuse, trainable=trainable)
-    coarse4 = conv2d('coarse4', coarse3, [3, 3, 384, 384], [384], [1, 1, 1, 1], padding='VALID', reuse=reuse, trainable=trainable)
-    coarse5 = conv2d('coarse5', coarse4, [3, 3, 384, 256], [256], [1, 1, 1, 1], padding='VALID', reuse=reuse, trainable=trainable)
-    coarse6 = fc('coarse6', coarse5, [6*10*256, 4096], [4096], reuse=reuse, trainable=trainable)
+    #input should be [n,240,320,3]
+    wd = 0.0
+    conv1 = conv2d('conv1', images, [7, 7, 3, 256], [256], [1, 16, 16, 1], padding='SAME', reuse=reuse, trainable=trainable)
+    conv1 = tf.contrib.layers.batch_norm(conv1)
+    multScalar = _variable_with_weight_decay(
+            'weights1',
+            shape=[1],
+            stddev=0.01,
+            wd=wd,
+            trainable=trainable
+        )
+    addScalar = _variable_with_weight_decay(
+            'weights2',
+            shape=[1],
+            stddev=0.01,
+            wd=wd,
+            trainable=trainable
+        )
+    conv1 = tf.multiply(conv1, multScalar)
+    conv1 = tf.add(conv1, addScalar)
+    conv1 = tf.nn.relu(conv1, 'relu')
+    print "conv1"
+    print conv1
+
+    #connect these two layers
+
+    coarse6 = fc('coarse6', conv1, [256, 4096], [4096], reuse=reuse, trainable=trainable)
     coarse7 = fc('coarse7', coarse6, [4096, 4070], [4070], reuse=reuse, trainable=trainable)
     coarse7_output = tf.reshape(coarse7, [-1, 55, 74, 1])
-
+    print "coarse7_output"
+    print coarse7_output
     return coarse7_output
-
-
-def inference_refine(images, coarse7_output, keep_conv, reuse=False, trainable=True):
-    fine1_conv = conv2d('fine1', images, [9, 9, 3, 63], [63], [1, 2, 2, 1], padding='VALID', reuse=reuse, trainable=trainable)
-    fine1 = tf.nn.max_pool(fine1_conv, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='fine_pool1')
-    fine1_dropout = tf.nn.dropout(fine1, keep_conv)
-    fine2 = tf.concat([fine1_dropout, coarse7_output], 3)
-    fine3 = conv2d('fine3', fine2, [5, 5, 64, 64], [64], [1, 1, 1, 1], padding='SAME', reuse=reuse, trainable=trainable)
-    fine3_dropout = tf.nn.dropout(fine3, keep_conv)
-    fine4 = conv2d('fine4', fine3_dropout, [5, 5, 64, 1], [1], [1, 1, 1, 1], padding='SAME', reuse=reuse, trainable=trainable)
-    return fine4
-
 
 def loss(logits, depths, invalid_depths):
     logits_flat = tf.reshape(logits, [-1, 55*74])
     depths_flat = tf.reshape(depths, [-1, 55*74])
+    print "logits_flat"
+    print logits_flat
+    print "depths_flat"
+    print depths_flat
     invalid_depths_flat = tf.reshape(invalid_depths, [-1, 55*74])
 
     predict = tf.multiply(logits_flat, invalid_depths_flat)
