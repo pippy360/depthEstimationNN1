c60c82cade81a5023653eec1da807599cca233db
Tom Murphy <murphyt7@tcd.ie>
2017-10-12 18:29:43 +0100

    Update model.py

--- model.py
+++ model.py
@@ -204,7 +204,7 @@ def loss(logits, depths, invalid_depths):
     print logits_flat
     print "depths_flat"
     print depths_flat
-    invalid_depths_flat = tf.reshape(invalid_depths, [-1, 55*74])
+    invalid_depths_flat = tf.reshape(invalid_depths, [-1, 120*160])
 
     predict = tf.multiply(logits_flat, invalid_depths_flat)
     target = tf.multiply(depths_flat, invalid_depths_flat)
@@ -213,7 +213,7 @@ def loss(logits, depths, invalid_depths):
     sum_square_d = tf.reduce_sum(square_d, 1)
     sum_d = tf.reduce_sum(d, 1)
     sqare_sum_d = tf.square(sum_d)
-    cost = tf.reduce_mean(sum_square_d / 55.0*74.0 - 0.5*sqare_sum_d / math.pow(55*74, 2))
+    cost = tf.reduce_mean(sum_square_d / 120*160.0 - 0.5*sqare_sum_d / math.pow(120*160, 2))
     tf.add_to_collection('losses', cost)
     return tf.add_n(tf.get_collection('losses'), name='total_loss')
 
