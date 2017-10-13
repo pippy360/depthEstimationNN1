cc57afaa28818c111804a7770023b8a4a9bc3dca
Tom Murphy <murphyt7@tcd.ie>
2017-10-13 13:57:48 +0100

    Create gpucommandstorun.py

---
+++ gpucommandstorun.py
@@ -0,0 +1,34 @@
+sudo apt-get update
+sudo apt-get install python-pip tmux git -y
+pip install tensorflow pillow
+
+git clone https://github.com/tensorflow/tensorflow 
+cd tensorflow
+sudo apt-get install python-numpy python-dev python-pip python-wheel
+sudo apt-get install libcupti-dev 
+sudo pip install six numpy wheel 
+
+# install CUDA Toolkit v8.0
+# instructions from https://developer.nvidia.com/cuda-downloads (linux -> x86_64 -> Ubuntu -> 16.04 -> deb (network))
+CUDA_REPO_PKG="cuda-repo-ubuntu1604_8.0.61-1_amd64.deb"
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
+sudo dpkg -i ${CUDA_REPO_PKG}
+sudo apt-get update
+sudo apt-get -y install cuda
+
+# install cuDNN v6.0
+CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
+wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/${CUDNN_TAR_FILE}
+tar -xzvf ${CUDNN_TAR_FILE}
+sudo cp -P cuda/include/cudnn.h /usr/local/cuda-8.0/include
+sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64/
+sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
+
+# set environment variables
+export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+
+bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package 
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+sudo pip install /tmp/tensorflow_pkg/tensorflow-1.3.0-py2-none-any.whl
