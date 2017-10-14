327fe390ebae4cdcffc1c9b9f182c3222b23898e
Tom Murphy <murphyt7@tcd.ie>
2017-10-14 14:31:25 +0100

    Update task.py

--- task.py
+++ task.py
@@ -32,21 +32,17 @@ def train():
         with tf.Session(config=tf.ConfigProto(log_device_placement=LOG_DEVICE_PLACEMENT)) as sess:
             sess.run(init_op)
             # parameters
-            coarse_params = {}
-            refine_params = {}
+            # Merge all the summaries and write them out to /tmp/mnist_logs (by default)
+            merged = tf.summary.merge_all()
+            train_writer = tf.summary.FileWriter(FLAGS.summaries_dir + '/train', sess.graph)
+            test_writer = tf.summary.FileWriter(FLAGS.summaries_dir + '/test')
+            tf.global_variables_initializer().run()
             
             for variable in tf.trainable_variables():
                 variable_name = variable.name
                 print("parameter: %s" %(variable_name))
                 if variable_name.find("/") < 0 or variable_name.count("/") != 1:
                     continue
-                if variable_name.find('coarse') >= 0:
-                    coarse_params[variable_name] = variable
-                if variable_name.find('fine') >= 0:
-                    refine_params[variable_name] = variable
-            # define saver
-            #print coarse_params
-            #saver_coarse = tf.train.Saver(coarse_params)
 
             # train
             coord = tf.train.Coordinator()
@@ -54,17 +50,17 @@ def train():
             for step in xrange(MAX_STEPS):
                 index = 0
                 for i in xrange(1000):
-                    _, loss_value, logits_val, images_val = sess.run([train_op, loss, logits, images], feed_dict={keep_conv: 0.8, keep_hidden: 0.5})
+                    summary, loss_value, logits_val, images_val = sess.run([train_op, loss, logits, images], feed_dict={keep_conv: 0.8, keep_hidden: 0.5})
                     if index % 10 == 0:
+                        test_writer.add_summary(summary, index)
                         print("%s: %d[epoch]: %d[iteration]: train loss %f" % (datetime.now(), step, index, loss_value))
                         assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
                     if index % 500 == 0:
                         output_predict(logits_val, images_val, "data/predict_%05d_%05d" % (step, i))
+
+                    train_writer.add_summary(summary, index)
                     index += 1
 
-                if step % 5 == 0 or (step * 1) == MAX_STEPS:
-                    coarse_checkpoint_path = COARSE_DIR + '/model.ckpt'
-                    saver_coarse.save(sess, coarse_checkpoint_path, global_step=step)
             coord.request_stop()
             coord.join(threads)
 
